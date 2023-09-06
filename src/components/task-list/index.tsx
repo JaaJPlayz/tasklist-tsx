5bb9ea10fac8126ed5dffa5b815fdfeeee5ad103
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-09-06 15:18:09 -0300

    Progress

--- src/components/task-list/index.tsx
+++ src/components/task-list/index.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { FiTrash } from 'react-icons/fi'
 import { BsPencil } from 'react-icons/bs'
+import {AiOutlineCheck} from 'react-icons/ai'
+import {AiOutlinePlusCircle} from 'react-icons/ai'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 
 const TaskList = () => {
@@ -33,6 +37,12 @@ const TaskList = () => {
     setTasks([]);
   };
 
+  const completeTask = (index: number) => {
+    const newTasks = [...tasks];
+    newTasks[index] += ' (completed)';
+    setTasks(newTasks);
+  };
+
   return (
     <div className='tasklist-div'>
       <form onSubmit={handleSubmit}>
@@ -40,16 +50,19 @@ const TaskList = () => {
           type="text"
           value={task}
           onChange={(e) => setTask(e.target.value)}
+          className="input-group"
+          placeholder="Add a task"
         />
-        <button type="submit">Add Task</button>
+        <button type="submit" className="btn btn-primary"><AiOutlinePlusCircle /></button>
       </form>
-      <button onClick={clearTasks}>Clear</button>
+      <button onClick={clearTasks} className="btn btn-danger">Clear</button>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
             {task}{' '}
-            <button onClick={() => handleEdit(index)}><BsPencil /></button>{' '}
-            <button onClick={() => handleDelete(index)}><FiTrash /></button>
+            <button onClick={() => completeTask(index)} className='btn btn-success'><AiOutlineCheck /></button>{' '}
+            <button onClick={() => handleEdit(index)} className='btn btn-primary'><BsPencil /></button>{' '}
+            <button onClick={() => handleDelete(index)} className='btn btn-danger'><FiTrash /></button>
           </li>
         ))}
       </ul>
